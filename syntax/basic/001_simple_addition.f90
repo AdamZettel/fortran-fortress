46b9493dcbf9a9159c68efdb29626dcccc86a182
AdamZettel <104731318+AdamZettel@users.noreply.github.com>
2024-07-08 11:17:14 -0400

    Create 001_simple_addition.f90

---
+++ syntax/basic/001_simple_addition.f90
@@ -0,0 +1,10 @@
+program simple_addition
+    implicit none
+    integer :: a, b, sum
+
+    a = 5
+    b = 10
+    sum = a + b
+
+    print *, "The sum of ", a, " and ", b, " is ", sum
+end program simple_addition
