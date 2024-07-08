3fad3f96e496963408860c8950716848e5131731
AdamZettel <104731318+AdamZettel@users.noreply.github.com>
2024-07-08 12:10:18 -0400

    Create 003_input_output.f90

---
+++ syntax/basic/003_input_output.f90
@@ -0,0 +1,8 @@
+program input_output
+    implicit none
+    integer :: num
+
+    print *, "Enter an integer (press enter after you type it):"
+    read *, num
+    print *, "You entered: ", num
+end program input_output
